56857a0d4486a495191a4fa1b899f3d7754c3a3f
CrackyStudio <antoine.nivoy@gmail.com>
2020-03-27 10:25:42 +0100

    Fix .env verification

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,25 +1,26 @@
 import React, { useEffect } from 'react';
+import { isEmpty } from 'lodash';
 
 import Home from 'screens/home';
 import InternalError from 'components/internalError';
 import Loader from 'components/loader';
 import { useStateValue } from 'hooks/provider';
-import { isFullFilled } from 'services/files';
 import 'styles/app.css';
 
 const App: React.FC = () => {
   const [{ isConfigured }, dispatch] = useStateValue() as Array<any>;
-  const neededValues: string[] = [];
+  const envValues: string[] = [];
 
   useEffect(() => {
     if (isConfigured === undefined) {
-      if (isFullFilled(neededValues)) {
+      const missingValues = envValues.filter((v: string): boolean => !process.env[`REACT_APP_${v}`]);
+      if (isEmpty(missingValues)) {
         dispatch({ key: 'isConfigured', value: true });
       } else {
         dispatch({ key: 'isConfigured', value: false });
       }
     }
-  }, [isConfigured, neededValues, dispatch]);
+  }, [isConfigured, envValues, dispatch]);
 
   return (
     <div className="App">
